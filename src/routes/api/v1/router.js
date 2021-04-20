a8368b76efeb07d18536fd90846687af6da3f364
Ida Rosvall <ir222gn@student.lnu.se>
2021-04-20 11:38:34 +0200

    Add gateway support for resource service requests

--- src/routes/api/v1/router.js
+++ src/routes/api/v1/router.js
@@ -1,5 +1,5 @@
 /**
- * Authenticaton router.
+ * API version 1 routes.
  *
  * @author Ida Rosvall <ir222gn@student.lnu.se
  * @version 1.0.0
@@ -7,7 +7,9 @@
 
 import express from 'express'
 import { router as authRouter } from './auth-router.js'
+import { router as resourceRouter } from './resource-router.js'
 
 export const router = express.Router()
 
 router.use('/auth', authRouter)
+router.use('/resource', resourceRouter)
