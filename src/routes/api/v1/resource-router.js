26cdf6bb4201408ff50e22069b35094efc10f0fe
Ida Rosvall <ir222gn@student.lnu.se>
2021-05-10 14:14:21 +0200

    Add support for friend requests and acceptance of them

--- src/routes/api/v1/resource-router.js
+++ src/routes/api/v1/resource-router.js
@@ -12,4 +12,14 @@ export const router = express.Router()
 
 const controller = new ResourceController()
 
+// ------------------------------------------------------------------------------
+//  Routes
+// ------------------------------------------------------------------------------
+
+// PATCH user/friendrequest/:userID
+router.patch('/user/friendrequest/:userID', (req, res, next) => controller.sendFriendRequest(req, res, next))
+
+// PATCH user/acceptfriend/:userID
+router.patch('/user/acceptfriend/:userID', (req, res, next) => controller.sendFriendRequest(req, res, next))
+
 router.all('*', (req, res, next) => controller.request(req, res, next))
