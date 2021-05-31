d75032765f1bab92270994538ccb91438d93d363
Ida Rosvall <ir222gn@student.lnu.se>
2021-05-31 17:09:13 +0200

    Make some routes unaccessible to resource service

--- src/routes/api/v1/resource-router.js
+++ src/routes/api/v1/resource-router.js
@@ -6,6 +6,7 @@
  */
 
 import express from 'express'
+import createError from 'http-errors'
 import { ResourceController } from '../../../controllers/api/resource-controller.js'
 
 export const router = express.Router()
@@ -15,6 +16,12 @@ const controller = new ResourceController()
 // ------------------------------------------------------------------------------
 //  Routes
 // ------------------------------------------------------------------------------
+
+// POST user is made not to work since it needs to go through auth service first.
+router.post('/user', (req, res, next) => next(createError(404)))
+
+// DELETE user is made not to work since it needs to go through auth service first.
+router.delete('/user', (req, res, next) => next(createError(404)))
 
 // PATCH user/friendrequest/:userID
 router.patch('/user/friendrequest/:userID', (req, res, next) => controller.sendFriendRequest(req, res, next))
